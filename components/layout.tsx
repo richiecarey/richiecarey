9cf235346fccbab225177d941cc36b92a6c2ee56
Richie Carey <richiecarey@gmail.com>
2022-07-20 14:23:46 -0400

    Updates.

--- components/layout.tsx
+++ components/layout.tsx
@@ -25,7 +25,7 @@ const Layout = ({ children }: Props) => {
                     {children}
                 </section>
             </main>
-            <Footer />
+            {/* <Footer /> */}
         </>
     );
 };
